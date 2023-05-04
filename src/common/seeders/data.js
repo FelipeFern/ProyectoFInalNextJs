395afcdcee2b68a33e1a6c4de7691b3ef737323c
Felipe Fernandez <ffernandez@liveperson.com>
2023-05-04 20:26:39 -0300

    Data seeders.

---
+++ src/common/seeders/data.js
@@ -0,0 +1,116 @@
+export const users = [
+	{
+		email: 'bart@gmail.com',
+		name: 'Bart',
+		apellido: 'Simpsons',
+		password: 'asd',
+		// sectorOmic: 'Atencion al cliente',
+		consultas: [],
+	},
+	{
+		email: 'homero@gmail.com',
+		name: 'Homer',
+		apellido: 'Simpsons',
+		password: 'asd',
+		// sectorOmic: '',
+		consultas: [],
+	},
+	{
+		email: 'lisa@gmail.com',
+		name: 'Lisa',
+		apellido: 'Simpsons',
+		password: 'asd',
+		// sectorOmic: '',
+		consultas: [],
+	},
+	{
+		email: 'marge@gmail.com',
+		name: 'Marge',
+		apellido: 'Simpsons',
+		password: 'asd',
+		// sectorOmic: '',
+		consultas: [],
+	},
+	{
+		email: 'maggie@gmail.com',
+		name: 'Maggie',
+		apellido: 'Simpsons',
+		password: 'asd',
+		// sectorOmic: '',
+		consultas: [],
+	},
+];
+
+export const sectorOmic = [
+	{
+		nombre: 'Abogados',
+		area: 'Audiencias - Conciliaciones',
+		empleados: [],
+	},
+	{
+		nombre: 'Director',
+		area: 'Direccion',
+		empleados: [],
+	},
+	{
+		nombre: 'Consultas',
+		area: 'Administracion',
+		empleados: [],
+	},
+	{
+		nombre: 'Audiencias conciliacion',
+		area: 'Administracion',
+		empleados: [],
+	},
+];
+
+export const Localidad = [
+	{
+		nombre: 'Bahia Blanca',
+		provincia: 'Buenos Aires',
+		empresasDenunciadas: [],
+		denunciantes: [],
+	},
+	{
+		nombre: 'Ingeniero White',
+		provincia: 'Buenos Aires',
+		empresasDenunciadas: [],
+		denunciantes: [],
+	},
+	{
+		nombre: 'General Daniel Cerri',
+		provincia: 'Buenos Aires',
+		empresasDenunciadas: [],
+		denunciantes: [],
+	},
+	{
+		nombre: 'Grünbein',
+		provincia: 'Buenos Aires',
+		empresasDenunciadas: [],
+		denunciantes: [],
+	},
+	{
+		nombre: 'Cabildo',
+		provincia: 'Buenos Aires',
+		empresasDenunciadas: [],
+		denunciantes: [],
+	},
+	{
+		nombre: 'Villa Espora',
+		provincia: 'Buenos Aires',
+		empresasDenunciadas: [],
+		denunciantes: [],
+	},
+	{
+		nombre: 'Villa Bordeu',
+		provincia: 'Buenos Aires',
+		empresasDenunciadas: [],
+		denunciantes: [],
+	},
+	{
+		nombre: 'La Vitícola',
+		provincia: 'Buenos Aires',
+		empresasDenunciadas: [],
+		denunciantes: [],
+	},
+];
