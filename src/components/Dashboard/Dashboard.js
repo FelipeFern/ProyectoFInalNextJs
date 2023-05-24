f58fe0932fe50fee1c51e85adf70be16a434b4dd
Felipe Fernandez <ffernandez@liveperson.com>
2023-05-23 21:50:16 -0300

    Divide components.

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -15,6 +15,7 @@ import HeaderDashboard from './HeaderDashboard';
 import Content from './Content';
 import ResultsCards from './ResultsCards';
 import Cards from './Cards';
+import Sidebar from './Sidebar';
 
 export default function Dashboard() {
 	const [openSidebar, setOpenSidebar] = useState(false);
@@ -24,89 +25,10 @@ export default function Dashboard() {
 		document.title = 'Dashboard';
 	}, []);
 
-	const handleSidebar = () => {
-		setOpenSidebar(!openSidebar);
-	};
-
 	return (
 		<div className='min-h-screen grid grid-col-1 lg:grid-cols-6'>
 			{/* Sidebar */}
-			<div
-				className={`fixed lg:static top-0 w-[81%] md:w-[40%] lg:w-full z-50 bg-white transition-all ${
-					openSidebar ? '-left-0' : '-left-full'
-				}  w-full h-full overflow-y-scroll col-span-1 p-8 border-r`}
-			>
-				<div className='text-center p-8'>
-					<h1 className='uppercase font-bold tracking-[4px]'>Tu Logo</h1>
-				</div>
-
-				<div className=' h-[800px] flex flex-col justify-between'>
-					{/* Menu */}
-					<nav>
-						<ul>
-							<li>
-								<Link
-									to='/'
-									className={`flex items-center gap-4  hover:{$hoverCollor} p-4 text-gray-500 hover:text-white rounded-lg transition-colors font-semibold`}
-								>
-									<RiMapPin2Line /> Localidades
-								</Link>
-							</li>
-							<li>
-								<Link
-									to='/'
-									className='flex items-center gap-4  hover:bg-purple-500 p-4 text-gray-500 hover:text-white rounded-lg transition-colors font-semibold'
-								>
-									<RiTable2 /> Sector OMIC
-								</Link>
-							</li>
-							<li>
-								<Link
-									to='/'
-									className='flex items-center gap-4  hover:bg-purple-500 p-4 text-gray-500 hover:text-white rounded-lg transition-colors font-semibold'
-								>
-									<RiBriefcase3Line /> Empresas
-								</Link>
-							</li>
-							<li>
-								<Link
-									to='/'
-									className='flex items-center gap-4  hover:bg-purple-500 p-4 text-gray-500 hover:text-white rounded-lg transition-colors font-semibold'
-								>
-									<RiFileUserLine /> Empleados
-								</Link>
-							</li>
-						</ul>
-					</nav>
-					{/* Images and logoout */}
-					<div className='flex flex-col gap-4'>
-						<img src='/images/img.svg' className='wf' alt='Image' />
-						<div className='bg-purple-100 p-8 flex flex-col gap-4 rounded-2xl'>
-							<h3 className='text-xl text-center'>Get upgrade</h3>
-							<p className='text-gray-500 text-center'>
-								Upgrade to a premium account today
-							</p>
-							<button className=' bg-purple-600 text-white p-2 rounded-lg'>
-								learn more
-							</button>
-						</div>
-						<Link
-							to='/'
-							className='flex items-center gap-4  hover:bg-purple-500 p-4 text-gray-500 hover:text-white rounded-lg transition-colors font-semibold'
-						>
-							<RiLogoutBoxRLine /> Logout
-						</Link>
-					</div>
-				</div>
-			</div>
-
-			{/* Btn menu movil */}
-			<button
-				onClick={() => handleSidebar()}
-				className=' block lg:hidden fixed bottom-4 right-4 bg-purple-600 text-white p-2 rounded-full text-2xl z-40'
-			>
-				{openSidebar ? <RiCloseLine /> : <RiMenu2Line />}
-			</button>
+			<Sidebar />
 			{/* Content */}
 			<div className=' col-span-5 bg-gray-200'>
 				<HeaderDashboard />
@@ -117,3 +39,6 @@ export default function Dashboard() {
 		</div>
 	);
 }
+
+// Ponerlo derecho> 2:53:00 explica.
+// Poner la barra izq y la de arriba fijas.
---
+++ src/components/Dashboard/Sidebar.js
@@ -0,0 +1,112 @@
+import {
+	RiDashboardLine,
+	RiBuilding4Line,
+	RiTable2,
+	RiBriefcase3Line,
+	RiFileUserLine,
+	RiLogoutBoxRLine,
+	RiMenu2Line,
+	RiCloseLine,
+	RiMapPin2Line,
+} from 'react-icons/ri';
+import { Link, Routes } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+
+export default function Sidebar() {
+	const [openSidebar, setOpenSidebar] = useState(false);
+	const hoverColor = 'bg-purple-500';
+
+	useEffect(() => {
+		document.title = 'Dashboard';
+	}, []);
+
+	const handleSidebar = () => {
+		setOpenSidebar(!openSidebar);
+	};
+
+	return (
+		<>
+			{/* Sidebar */}
+			<div
+				className={`fixed lg:static top-0 w-[81%] md:w-[40%] lg:w-full z-50 bg-white transition-all ${
+					openSidebar ? '-left-0' : '-left-full'
+				}  w-full h-full overflow-y-scroll col-span-1 p-8 border-r`}
+			>
+				<div className='text-center p-8'>
+					<h1 className='uppercase font-bold tracking-[4px]'>Tu Logo</h1>
+				</div>
+
+				<div className=' h-[800px] flex flex-col justify-between'>
+					{/* Menu */}
+					<nav>
+						<ul>
+							<li>
+								<Link
+									to='/'
+									className={`flex items-center gap-4  hover:${hoverColor} p-4 text-gray-500 hover:text-white rounded-lg transition-colors font-semibold`}
+								>
+									<RiMapPin2Line /> Localidades
+								</Link>
+							</li>
+							<li>
+								<Link
+									to='/'
+									className='flex items-center gap-4  hover:bg-purple-500 p-4 text-gray-500 hover:text-white rounded-lg transition-colors font-semibold'
+								>
+									<RiTable2 /> Sector OMIC
+								</Link>
+							</li>
+							<li>
+								<Link
+									to='/'
+									className='flex items-center gap-4  hover:bg-purple-500 p-4 text-gray-500 hover:text-white rounded-lg transition-colors font-semibold'
+								>
+									<RiBriefcase3Line /> Empresas
+								</Link>
+							</li>
+							<li>
+								<Link
+									to='/'
+									className='flex items-center gap-4  hover:bg-purple-500 p-4 text-gray-500 hover:text-white rounded-lg transition-colors font-semibold'
+								>
+									<RiFileUserLine /> Empleados
+								</Link>
+							</li>
+						</ul>
+					</nav>
+					{/* Images and logoout */}
+					<div className='flex flex-col gap-4'>
+						<img src='/images/img.svg' className='wf' alt='Image' />
+						<div className='bg-purple-100 p-8 flex flex-col gap-4 rounded-2xl'>
+							<h3 className='text-xl text-center'>Get upgrade</h3>
+							<p className='text-gray-500 text-center'>
+								Upgrade to a premium account today
+							</p>
+							<button className=' bg-purple-600 text-white p-2 rounded-lg'>
+								learn more
+							</button>
+						</div>
+						<Link
+							to='/'
+							className='flex items-center gap-4  hover:bg-purple-500 p-4 text-gray-500 hover:text-white rounded-lg transition-colors font-semibold'
+						>
+							<RiLogoutBoxRLine /> Logout
+						</Link>
+					</div>
+				</div>
+			</div>
+
+			{/* Btn menu movil */}
+			<button
+				onClick={() => handleSidebar()}
+				className=' block lg:hidden fixed bottom-4 right-4 bg-purple-600 text-white p-2 rounded-full text-2xl z-40'
+			>
+				{openSidebar ? <RiCloseLine /> : <RiMenu2Line />}
+			</button>
+			{/* Content */}
+		</>
+	);
+}
+
+// Ponerlo derecho> 2:53:00 explica.
+// Poner la barra izq y la de arriba fijas.
