37ea0dd696421ec20d09c1558e4e7b4f940304f8
Fernandez Felipe <felipefernandz@hotmail.com>
2023-09-11 23:23:32 -0300

    Multer working to upload files.

--- src/pages/api/solicitudes/tiposSolicitudes/index.js
+++ src/pages/api/solicitudes/tiposSolicitudes/index.js
@@ -7,10 +7,19 @@ import {
 	addDoc,
 	updateDoc,
 } from 'firebase/firestore';
-import { getStorage, ref, uploadBytes } from 'firebase/storage';
+import {
+	getStorage,
+	ref,
+	uploadBytes,
+	uploadBytesResumable,
+	put,
+} from 'firebase/storage';
 import APIRouteHelper from '../../../../common/api/APIRouteHelper';
 import { db } from '../../../../common/db/firebase';
 import formidable, { formidableErrors } from 'formidable';
+import multer from 'multer';
+
+const upload = multer({ dest: 'uploads/' });
 
 export default async function (req, res) {
 	return new APIRouteHelper(req, res, permissionName.API_tipoSolicitudes)
@@ -36,25 +45,136 @@ async function onGET(req, res) {
 	}
 }
 
+// async function onPOST(req, res) {
+// 	try {
+// 		const form = formidable({});
+
+// 		form.parse(req, async (err, fields, files) => {
+// 			let {
+// 				nombre,
+// 				apellido,
+// 				dni,
+// 				cuil,
+// 				telefonoCelular,
+// 				telefonoFijo,
+// 				domicilioCalle,
+// 				domicilioNumero,
+// 				domicilioPiso,
+// 				email,
+// 				localidad,
+// 				empresa,
+// 				tipoConsulta,
+// 			} = fields;
+
+// 			console.log(fields);
+// 			if (err) {
+// 				console.log(err);
+// 				res.status(500).json({ error: 'Error al procesar el formulario' });
+// 				return;
+// 			}
+
+// 			const rutaAlmacenamiento = `${localidad}/${dni}/${empresa}`;
+// 			const storage = getStorage();
+
+// 			for (const key in files) {
+// 				if (Object.hasOwnProperty.call(files, key)) {
+// 					const file = files[key];
+// 					const fileName = file.name; // Nombre original del archivo
+
+// 					// Construye la referencia de almacenamiento con la ruta personalizada
+// 					const fileRef = ref(storage, `${rutaAlmacenamiento}${fileName}`);
+
+// 					// Sube el archivo a Firebase Storage
+// 					await uploadBytes(fileRef, file.path);
+
+// 					console.log(`Archivo ${fileName} cargado en ${rutaAlmacenamiento}`);
+// 				}
+// 			}
+
+// 			// for (let file of files) {
+// 			// 	console.log(file);
+// 			// }
+
+// 			// Hay que guardar los archivos en la BD
+
+// 			// console.log('Campos:', fields);
+// 			// console.log('Archivos:', files);
+// 			// Envía la respuesta una vez que hayas terminado de procesar.
+// 			res.status(200).json({ mensaje: 'Procesamiento completado' });
+// 		});
+// 	} catch (error) {
+// 		res.status(500).json({ error: 'Error al procesar' });
+// 	}
+// }
+
 async function onPOST(req, res) {
 	try {
-		const form = formidable({});
+		console.log('Entre');
 
-		form.parse(req, (err, fields, files) => {
-			console.log(fields);
-			if (err) {
-				console.log(err);
-				res.status(500).json({ error: 'Error al procesar el formulario' });
+		const processForm = async () => {
+			await new Promise(async (resolve, reject) => {
+				const upload = multer().array('archivos', 5);
+				await upload(req, res, (err) => {
+					if (err) {
+						console.error(err);
+						res.status(500).json({ error: 'Error al procesar el formulario' });
+						reject(err);
+					} else {
+						resolve();
+					}
+				});
+			});
+			// Accede a los archivos cargados a través de req.files, que será un array.
+			const archivos = req.files;
+			const {
+				nombre,
+				apellido,
+				dni,
+				cuil,
+				telefonoCelular,
+				telefonoFijo,
+				domicilioCalle,
+				domicilioNumero,
+				domicilioPiso,
+				email,
+				localidad,
+				empresa,
+				tipoConsulta,
+			} = req.body;
+
+			// Verifica si se cargaron archivos.
+			if (!archivos || archivos.length === 0) {
+				res.status(400).json({ error: 'No se han proporcionado archivos.' });
 				return;
 			}
 
-			// Realiza cualquier procesamiento necesario con fields y files aquí.
-			console.log('Campos:', fields);
-			console.log('Archivos:', files);
-			// Envía la respuesta una vez que hayas terminado de procesar.
+			const rutaAlmacenamiento = `${localidad}/${dni}/${empresa}/`;
+			const storage = getStorage();
+
+			// Itera sobre los archivos y realiza operaciones con cada uno.
+			for (const file of archivos) {
+				const fileName = file.originalname;
+				const fileBuffer = file.buffer; // Búfer del archivo
+
+				const metadata = {
+					contentType: file.mimetype,
+				};
+
+				// Construye la referencia de almacenamiento con la ruta personalizada
+				const fileRef = ref(storage, `${rutaAlmacenamiento}${fileName}`);
+
+				// Sube el archivo a Firebase Storage
+				await uploadBytes(fileRef, fileBuffer, metadata);
+
+				console.log(`Archivo ${fileName} cargado en ${rutaAlmacenamiento}`);
+			}
+
 			res.status(200).json({ mensaje: 'Procesamiento completado' });
-		});
+		};
+
+		await processForm();
 	} catch (error) {
+		console.log(error);
 		res.status(500).json({ error: 'Error al procesar' });
 	}
 }
