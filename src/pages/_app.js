8d30078188843cd0454fb8a7308aebe67aba84c5
Fernandez Felipe <felipefernandz@hotmail.com>
2023-10-19 19:38:16 -0300

    _app updated.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,6 @@
 import { AuthProvider } from '../common/context/AuthContext';
 import '../styles/globals.css';
-import { BrowserRouter } from 'react-router-dom';
 import React, { useEffect } from 'react';
-import Dashboard from '@/components/DashboardLocalidades/Dashboard';
-import Head from 'next/head';
-import MainLayout from '@/components/Layout/MainLayout';
 import { initFirebaseClient } from '@/common/db/firebase';
 import { SessionProvider } from 'next-auth/react';
 import { ConsultasProvider } from '@/context/ConsultasContext';
@@ -13,28 +9,16 @@ export default function App({
 	Component,
 	pageProps: { session, ...pageProps },
 }) {
-	// return (
-	// 	<AuthProvider>
-	// 		<Layout>
-	// 			<Component {...pageProps} />
-	// 		</Layout>
-	// 	</AuthProvider>
-	// );
 
 	initFirebaseClient();
 
 	return (
 		<>
-			{/* <Head>
-				<title>OMIC</title>
-			</Head>*/}
-			{/* <MainLayout> */}
 			<SessionProvider session={session}>
 				<ConsultasProvider>
 					<Component {...pageProps} />
 				</ConsultasProvider>
 			</SessionProvider>
-			{/* </MainLayout> */}
 		</>
 	);
 }
