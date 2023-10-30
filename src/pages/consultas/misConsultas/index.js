b915898b8b22e04786458e88c47acaef784923c5
Fernandez Felipe <felipefernandz@hotmail.com>
2023-10-29 23:40:25 -0300

    MisConsultas segured.

---
+++ src/pages/consultas/misConsultas/index.js
@@ -0,0 +1,25 @@
+import ConsultasLayout from '@/layouts/ConsultasLayout';
+import { useConsultasContext } from '@/context/ConsultasContext';
+import { useSession, signOut } from 'next-auth/react';
+import { useRouter } from 'next/router';
+
+export default function ConsultasPage() {
+	const { data: session, status, update } = useSession();
+	const { allConsultas } = useConsultasContext();
+	const router = useRouter();
+
+	let consultas = [];
+	if (session !== undefined) {
+		let id = session.user.id;
+		consultas = allConsultas.filter((objeto) => objeto.responsable === id);
+	}
+
+	const isAuthenticated = session?.user && session.user.role === 'Ciudadano';
+
+	if (!isAuthenticated) {
+		router.push('/404');
+		return null;
+	}
+
+	return <ConsultasLayout allConsultas={consultas} query='' />;
+}
